63dcd069fc0ba32a6666e5f79e87e6a1bc78118d
Bassel Madarati <basselmadarati@gmail.com>
2024-02-08 11:16:08 +0300

    Setting up the static routes

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,8 +1,8 @@
 #restaurant/urls.py
 from django.contrib import admin 
 from django.urls import path 
-from .views import sayHello 
+from . import views
   
 urlpatterns = [ 
-    path('', sayHello, name='sayHello'), 
+    path('', views.index, name='index'), 
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+# Create your views here.
  
 def sayHello(request):
- return HttpResponse('Hello World')
+    return HttpResponse('Hello World')
 
+def index(request):
+    return render(request, 'index.html', {})
